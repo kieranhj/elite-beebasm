8caefbe740b08ada7244634727ebe045faa394ac
Mark Moxon <mark@moxon.net>
2020-05-26 16:49:22 +0100

    Updated Python scripts to work with Python 3 as well as Python 2

--- crc32.py
+++ crc32.py
@@ -1,18 +1,19 @@
+from __future__ import print_function
 import sys,os,os.path,glob,zlib
 
 def main():
     folder=sys.argv[1] if len(sys.argv)>=2 else "."
     names=os.listdir(folder)
 
-    print ' CRC 32     File Size   Filename'
-    print '-------------------------------------------------'
+    print(' CRC 32     File Size   Filename')
+    print('-------------------------------------------------')
     
     for name in names:
         full_name=os.path.join(folder,name)
         if not os.path.isfile(full_name): continue
         with open(full_name,'rb') as f: data=f.read()
-        print '%08x  %9d  %s'%(zlib.crc32(data)&0xffffffff,
+        print ('%08x  %9d  %s'%(zlib.crc32(data)&0xffffffff,
                                 len(data),
-                                full_name)
+                                full_name))
 
 if __name__=='__main__': main()
